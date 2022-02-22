e5f3d95ed44e60ddddc0ca71691eae191a5c5f46
sadah <sadaaki.hirai@gmail.com>
2022-02-22 22:17:17 +0900

    Implement FieldElement

--- ecc/field_element.go
+++ ecc/field_element.go
@@ -1,7 +1,9 @@
 // Package ecc (Elliptic Curve Cryptography)
 package ecc
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type FieldElement struct {
 	num   int64
@@ -15,6 +17,10 @@ func NewFieldElement(num, prime int64) (*FieldElement, error) {
 	return &FieldElement{num, prime}, nil
 }
 
+func (fe *FieldElement) String() string {
+	return fmt.Sprintf("FieldElement_%v(%v)", fe.prime, fe.num)
+}
+
 func (fe *FieldElement) Equal(other *FieldElement) bool {
 	if other == nil {
 		return false
@@ -24,4 +30,70 @@ func (fe *FieldElement) Equal(other *FieldElement) bool {
 
 func (fe *FieldElement) NotEqual(other *FieldElement) bool {
 	return !fe.Equal(other)
+}
+
+func (fe *FieldElement) Add(other *FieldElement) (*FieldElement, error) {
+	if fe.prime != other.prime {
+		return nil, fmt.Errorf("cannot add two numbers in different Fields")
+	}
+	num := (fe.num + other.num) % fe.prime
+	return NewFieldElement(num, fe.prime)
+}
+
+func (fe *FieldElement) Sub(other *FieldElement) (*FieldElement, error) {
+	if fe.prime != other.prime {
+		return nil, fmt.Errorf("cannot subtract two numbers in different Fields")
+	}
+	num := (fe.num - other.num) % fe.prime
+	if num < 0 {
+		num += fe.prime
+	}
+	return NewFieldElement(num, fe.prime)
+}
+
+func (fe *FieldElement) Mul(other *FieldElement) (*FieldElement, error) {
+	if fe.prime != other.prime {
+		return nil, fmt.Errorf("cannot multiply two numbers in different Fields")
+	}
+	num := (fe.num % fe.prime) * (other.num % fe.prime) % fe.prime
+	return NewFieldElement(num, fe.prime)
+}
+
+func (fe *FieldElement) Pow(exponent int64) (*FieldElement, error) {
+	if exponent == 0 {
+		return NewFieldElement(1, fe.prime)
+	}
+	// use Fermat's little theorem
+	n := exponent % (fe.prime - 1)
+	if n < 0 {
+		n += fe.prime - 1
+	}
+	var t *FieldElement
+	var err error
+	t, err = NewFieldElement(fe.num, fe.prime)
+	if err != nil {
+		return nil, err
+	}
+	// won't use big numbers
+	for i := 1; i < int(n); i++ {
+		t, err = t.Mul(fe)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return t, nil
+}
+
+func (fe *FieldElement) Div(other *FieldElement) (*FieldElement, error) {
+	if fe.prime != other.prime {
+		return nil, fmt.Errorf("cannot divide two numbers in different Fields")
+	}
+	if other.num == 0 {
+		return nil, fmt.Errorf("cannot divide by zero")
+	}
+	pow, err := other.Pow(fe.prime - 2)
+	if err != nil {
+		return nil, err
+	}
+	return fe.Mul(pow)
 }
--- ecc/field_element_test.go
+++ ecc/field_element_test.go
@@ -1,6 +1,53 @@
 package ecc
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewFieldElement(t *testing.T) {
+	type args struct {
+		num   int64
+		prime int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *FieldElement
+		wantErr bool
+	}{
+		{
+			name:    "OK",
+			args:    args{0, 31},
+			want:    &FieldElement{0, 31},
+			wantErr: false,
+		},
+		{
+			name:    "OK",
+			args:    args{1, 31},
+			want:    &FieldElement{1, 31},
+			wantErr: false,
+		},
+		{
+			name:    "OK",
+			args:    args{32, 31},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewFieldElement(tt.args.num, tt.args.prime)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewFieldElement() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewFieldElement() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
 func TestFieldElement_Equal(t *testing.T) {
 	type fields struct {
@@ -16,6 +63,12 @@ func TestFieldElement_Equal(t *testing.T) {
 		args   args
 		want   bool
 	}{
+		{
+			name:   "OK",
+			fields: fields{0, 31},
+			args:   args{other: &FieldElement{0, 31}},
+			want:   true,
+		},
 		{
 			name:   "OK",
 			fields: fields{2, 31},
@@ -78,7 +131,7 @@ func TestFieldElement_NotEqual(t *testing.T) {
 			name:   "NG",
 			fields: fields{2, 31},
 			args:   args{other: nil},
-			want:   false,
+			want:   true,
 		},
 	}
 	for _, tt := range tests {
@@ -89,6 +142,288 @@ func TestFieldElement_NotEqual(t *testing.T) {
 			}
 			if got := fe.NotEqual(tt.args.other); got != tt.want {
 				t.Errorf("NotEqual() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFieldElement_Add(t *testing.T) {
+	type fields struct {
+		num   int64
+		prime int64
+	}
+	type args struct {
+		other *FieldElement
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *FieldElement
+		wantErr bool
+	}{
+		{
+			name:    "OK",
+			fields:  fields{2, 31},
+			args:    args{other: &FieldElement{15, 31}},
+			want:    &FieldElement{17, 31},
+			wantErr: false,
+		},
+		{
+			name:    "OK",
+			fields:  fields{17, 31},
+			args:    args{other: &FieldElement{21, 31}},
+			want:    &FieldElement{7, 31},
+			wantErr: false,
+		},
+		{
+			name:    "OK",
+			fields:  fields{0, 31},
+			args:    args{other: &FieldElement{21, 31}},
+			want:    &FieldElement{21, 31},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fe := &FieldElement{
+				num:   tt.fields.num,
+				prime: tt.fields.prime,
+			}
+			got, err := fe.Add(tt.args.other)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Add() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Add() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFieldElement_Sub(t *testing.T) {
+	type fields struct {
+		num   int64
+		prime int64
+	}
+	type args struct {
+		other *FieldElement
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *FieldElement
+		wantErr bool
+	}{
+		{
+			name:    "OK",
+			fields:  fields{29, 31},
+			args:    args{other: &FieldElement{4, 31}},
+			want:    &FieldElement{25, 31},
+			wantErr: false,
+		},
+		{
+			name:    "OK",
+			fields:  fields{15, 31},
+			args:    args{other: &FieldElement{30, 31}},
+			want:    &FieldElement{16, 31},
+			wantErr: false,
+		},
+		{
+			name:    "OK",
+			fields:  fields{0, 31},
+			args:    args{other: &FieldElement{30, 31}},
+			want:    &FieldElement{1, 31},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fe := &FieldElement{
+				num:   tt.fields.num,
+				prime: tt.fields.prime,
+			}
+			got, err := fe.Sub(tt.args.other)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Sub() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Sub() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFieldElement_Mul(t *testing.T) {
+	type fields struct {
+		num   int64
+		prime int64
+	}
+	type args struct {
+		other *FieldElement
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *FieldElement
+		wantErr bool
+	}{
+		{
+			name:    "OK",
+			fields:  fields{24, 31},
+			args:    args{other: &FieldElement{19, 31}},
+			want:    &FieldElement{22, 31},
+			wantErr: false,
+		},
+		{
+			name:    "OK",
+			fields:  fields{0, 31},
+			args:    args{other: &FieldElement{1, 31}},
+			want:    &FieldElement{0, 31},
+			wantErr: false,
+		},
+		{
+			name:    "OK",
+			fields:  fields{1, 31},
+			args:    args{other: &FieldElement{0, 31}},
+			want:    &FieldElement{0, 31},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fe := &FieldElement{
+				num:   tt.fields.num,
+				prime: tt.fields.prime,
+			}
+			got, err := fe.Mul(tt.args.other)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Mul() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Mul() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFieldElement_Pow(t *testing.T) {
+	type fields struct {
+		num   int64
+		prime int64
+	}
+	type args struct {
+		exponent int64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *FieldElement
+		wantErr bool
+	}{
+		{
+			name:    "OK",
+			fields:  fields{17, 31},
+			args:    args{exponent: 3},
+			want:    &FieldElement{15, 31},
+			wantErr: false,
+		},
+		{
+			name:    "OK",
+			fields:  fields{7, 31},
+			args:    args{exponent: -3},
+			want:    &FieldElement{16, 31},
+			wantErr: false,
+		},
+		{
+			name:    "OK",
+			fields:  fields{7, 31},
+			args:    args{exponent: 1},
+			want:    &FieldElement{7, 31},
+			wantErr: false,
+		},
+		{
+			name:    "OK",
+			fields:  fields{7, 31},
+			args:    args{exponent: 0},
+			want:    &FieldElement{1, 31},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fe := &FieldElement{
+				num:   tt.fields.num,
+				prime: tt.fields.prime,
+			}
+			got, err := fe.Pow(tt.args.exponent)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Pow() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Pow() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFieldElement_Div(t *testing.T) {
+	type fields struct {
+		num   int64
+		prime int64
+	}
+	type args struct {
+		other *FieldElement
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *FieldElement
+		wantErr bool
+	}{
+		{
+			name:    "OK",
+			fields:  fields{3, 31},
+			args:    args{other: &FieldElement{24, 31}},
+			want:    &FieldElement{4, 31},
+			wantErr: false,
+		},
+		{
+			name:    "OK",
+			fields:  fields{0, 31},
+			args:    args{other: &FieldElement{24, 31}},
+			want:    &FieldElement{0, 31},
+			wantErr: false,
+		},
+		{
+			name:    "NG",
+			fields:  fields{3, 31},
+			args:    args{other: &FieldElement{0, 31}},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fe := &FieldElement{
+				num:   tt.fields.num,
+				prime: tt.fields.prime,
+			}
+			got, err := fe.Div(tt.args.other)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Div() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Div() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
